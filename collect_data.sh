8d59af66ff64ad56e86748469983e69b5adf871f
printeger <printeger@gmail.com>
2024-05-28 16:50:34 +0800

    0528 collect data.

--- collect_data.sh
+++ collect_data.sh
@@ -3,25 +3,23 @@
 echo "Launching roscore..."
 gnome-terminal --tab -- bash -c "cd ~/ws_uav_setup; source devel/setup.bash; roscore; exec bash" 
 
-echo "Launching Mavros..."
-gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch --wait mavros px4.launch; exec bash"
-
 echo "Launching LiDAR..."
 gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch --wait livox_ros_driver2 msg_MID360.launch; exec bash"
 
 echo "Launching Fast-LIO..."
 gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch --wait fast_lio mapping_mid360_nrviz.launch; exec bash"
 
+echo "Launching RTKLIB..."
+gnome-terminal --tab -- bash -c "cd ~/deps/RTKLIB/app/consapp/str2str/gcc; ./str2str -in ntrip://polyu_th:poly61@landsd-gncaster.realtime.data.gov.hk:2101/HKSC_32 -out tcpsvr://:3503; exec bash"
+
 echo "Launching GNSS..."
-gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch ublox_driver ublox_driver.launch; exec bash"
+gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch --wait ublox_driver ublox_driver.launch; exec bash"
 
 echo "Launching UWB..."
 gnome-terminal --tab -- bash -c "source devel/setup.bash; roslaunch --wait nlink_parser linktrack.launch; exec bash"
 
-echo "Launching Camera..."
-gnome-terminal --tab -- bash -c "source devel/setup.bash; exec bash"
-
-
+# echo "Launching Camera..."
+# gnome-terminal --tab -- bash -c "source devel/setup.bash; exec bash"
 
 # echo "Validing..."
 # gnome-terminal --tab -- bash -c "source devel/setup.bash; rqt_graph; exec bash"
